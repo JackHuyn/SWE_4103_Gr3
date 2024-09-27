981e083c7d8d3b3e8e6f89852fd9e0cbc3d0c0f1
WMazerol <114442203+WMazerol@users.noreply.github.com>
2024-09-27 14:17:54 -0300

    Moved Auth Functions to Separate Class

---
+++ Backend/Auth.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+class FirebaseAuth:
+
+    def __init__(self, auth, api_key) -> None:
+        self.auth = auth
+        self.api_key = api_key
+        self.active_sessions = {}
+
+
+    def sign_up_with_email_and_password(self, fname, lname, email, password):
+        user = self.auth.create_user(
+        email=email,
+        email_verified=False,
+        password=password,
+        display_name=(str(fname) + ' ' + str(lname)),
+        disabled=False)
+        return user
+
+    def sign_in_with_email_and_password(self, email, password, return_secure_token=True):
+        payload = json.dumps({"email":email, "password":password, "return_secure_token":return_secure_token})
+        rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+
+        r = requests.post(rest_api_url,
+                    params={"key": self.api_key},
+                    data=payload)
+        login_resp = r.json()
+
+        self.active_sessions.update({login_resp['localId']: login_resp})
+
+        return login_resp
+    
+    def end_session(self, local_id):
+        try:
+            self.active_sessions.pop(local_id)
+            return True
+        except KeyError as ke:
+            return False
+
+    def validate_token(self, local_id, id_token):
+        if(local_id not in self.active_sessions):
+            print("LOCAL ID NOT FOUND")
+            return False
+        user_session = self.active_sessions[local_id]
+        if(user_session['idToken'] != id_token):
+            print("ID TOKEN DOES NOT MATCH")
+            end_session(local_id)
+            return False
+        try:
+            decoded_token = self.auth.verify_id_token(id_token)
+        except Exception as e:
+            print("INVALID TOKEN")
+            end_session(local_id)
+            return False
+        uid = decoded_token['uid']
+        return True
--- Backend/Backend.py
+++ Backend/Backend.py
@@ -4,6 +4,7 @@ import firebase_admin
 from firebase_admin import auth, credentials, firestore
 import json
 import requests
+import Auth as fb_auth
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,56 +12,11 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 FIREBASE_WEB_API_KEY = 'AIzaSyD-f3Vq6kGVXcfjnMmXFuoP1T1mRx7VJXo'
 
-cred = credentials.Certificate("C:/Users/willm/Documents/GitHub/SWE_4103_Gr3/swe4103-7b261-firebase-adminsdk-7nzkx-e88172454d.json")
+cred = credentials.Certificate("C:/Users/willm/Documents/GitHub/SWE_4103_Gr3/Backend/swe4103-7b261-firebase-adminsdk-7nzkx-e88172454d.json")
 firebase_admin.initialize_app(cred)
 ##db = firestore.client()
 
-active_sessions = {}
-
-
-def sign_up_with_email_and_password(fname, lname, email, password, return_secure_token=True):
-    user = auth.create_user(
-    email=email,
-    email_verified=False,
-    password=password,
-    display_name=(str(fname) + ' ' + str(lname)),
-    disabled=False)
-    return user
-
-def sign_in_with_email_and_password(email, password, return_secure_token=True):
-    payload = json.dumps({"email":email, "password":password, "return_secure_token":return_secure_token})
-    rest_api_url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
-
-    r = requests.post(rest_api_url,
-                  params={"key": FIREBASE_WEB_API_KEY},
-                  data=payload)
-
-    return r.json()
-
-def validate_token(local_id, id_token):
-    if(local_id not in active_sessions):
-        print("LOCAL ID NOT FOUND")
-        return False
-    user_session = active_sessions[local_id]
-    if(user_session['idToken'] != id_token):
-        print("ID TOKEN DOES NOT MATCH")
-        end_session(local_id)
-        return False
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-    except Exception as e:
-        print("INVALID TOKEN")
-        end_session(local_id)
-        return False
-    uid = decoded_token['uid']
-    return True
-
-def end_session(local_id):
-    try:
-        active_sessions.pop(local_id)
-        return True
-    except KeyError as ke:
-        return False
+firebase_auth = fb_auth.FirebaseAuth(auth, FIREBASE_WEB_API_KEY)
 
 
 @app.route('/welcome', methods=['GET'])
@@ -85,7 +41,7 @@ def signup_user():
     lname = request.args.get("lname", default = -1, type = str)
     email = request.args.get("email", default = -1, type = str)
     password = request.args.get("password", default = -1, type = str)
-    signup_resp = sign_up_with_email_and_password(fname, lname, email, password)
+    signup_resp = firebase_auth.sign_up_with_email_and_password(fname, lname, email, password)
     print(signup_resp)
     print('Login')
     return 'signup'
@@ -95,9 +51,7 @@ def signup_user():
 def login_user():
     email = request.args.get("email", default = -1, type = str)
     password = request.args.get("password", default = -1, type = str)
-    login_resp = sign_in_with_email_and_password(email, password)
-    print('Login')
-    active_sessions.update({login_resp['localId']: login_resp})
+    login_resp = firebase_auth.sign_in_with_email_and_password(email, password)
     response = app.response_class(
         response=json.dumps({'localId': login_resp['localId'], 'idToken': login_resp['idToken']}),
         status=200,
@@ -112,7 +66,7 @@ def validate_session():
     local_id = request.args.get("localId", default = -1, type = str)
     id_token = request.args.get("idToken", default = -1, type = str)
     response = app.response_class(
-        response=json.dumps({'approved': validate_token(local_id, id_token)}),
+        response=json.dumps({'approved': firebase_auth.validate_token(local_id, id_token)}),
         status=200,
         mimetype='application/json'
     )
@@ -123,9 +77,7 @@ def validate_session():
 @cross_origin()
 def logout_user():
     local_id = request.args.get("localId", default = -1, type = str)
-    print('Num Active Sessions Before: ' + str(len(active_sessions)))
-    end_session(local_id)
-    print('Num Active Sessions After: ' + str(len(active_sessions)))
+    firebase_auth.end_session(local_id)
     response = app.response_class(
         response=json.dumps({}),
         status=200,
