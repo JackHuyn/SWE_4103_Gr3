bbfd542c98fb028065a5ed848b7462616e8e747c
WMazerol <114442203+WMazerol@users.noreply.github.com>
2024-10-02 09:07:58 -0300

    Better Backend Error Responses

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -24,16 +24,6 @@ firebase_auth = fb_auth.FirebaseAuth(auth, FIREBASE_WEB_API_KEY)
 def get_welcome():
     return 'Welcome!'
 
-@app.route('/secure', methods=['GET'])
-@cross_origin()
-def get_secure():
-    token = request.args.get("sessionId", default = -1, type = str)
-    print(token)
-    if(token == -1):
-        print("Err")
-        pass
-    return 'Welcome Secure!'
-
 @app.route('/auth/signup-with-email-and-password', methods=['POST'])
 @cross_origin()
 def signup_user():
@@ -41,22 +31,41 @@ def signup_user():
     lname = request.args.get("lname", default = -1, type = str)
     email = request.args.get("email", default = -1, type = str)
     password = request.args.get("password", default = -1, type = str)
-    signup_resp = firebase_auth.sign_up_with_email_and_password(fname, lname, email, password)
-    print(signup_resp)
-    print('Login')
-    return 'signup'
+    try:
+        signup_resp = firebase_auth.sign_up_with_email_and_password(fname, lname, email, password)
+        print(signup_resp)
+        response = app.response_class(
+            response=json.dumps({'approved': True}),
+            status=(200),
+            mimetype='application/json'
+        )
+        return response
+    except Exception as e:
+        response = app.response_class(
+            response=json.dumps({'approved': False}),
+            status=(500),
+            mimetype='application/json'
+        )
+        return response
 
 @app.route('/auth/login-with-email-and-password', methods=['GET'])
 @cross_origin()
 def login_user():
     email = request.args.get("email", default = -1, type = str)
     password = request.args.get("password", default = -1, type = str)
-    login_resp = firebase_auth.sign_in_with_email_and_password(email, password)
-    response = app.response_class(
-        response=json.dumps({'localId': login_resp['localId'], 'idToken': login_resp['idToken']}),
-        status=200,
-        mimetype='application/json'
-    )
+    try:
+        login_resp = firebase_auth.sign_in_with_email_and_password(email, password)
+        response = app.response_class(
+            response=json.dumps({'approved': True, 'localId': login_resp['localId'], 'idToken': login_resp['idToken']}),
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception as e:
+        response = app.response_class(
+            response=json.dumps({'approved': False}),
+            status=401,
+            mimetype='application/json'
+        )
     print(response.response)
     return response
 
@@ -65,9 +74,10 @@ def login_user():
 def validate_session():
     local_id = request.args.get("localId", default = -1, type = str)
     id_token = request.args.get("idToken", default = -1, type = str)
+    valid = firebase_auth.validate_token(local_id, id_token)
     response = app.response_class(
-        response=json.dumps({'approved': firebase_auth.validate_token(local_id, id_token)}),
-        status=200,
+        response=json.dumps({'approved': valid}),
+        status=(200 if valid else 401),
         mimetype='application/json'
     )
     print(response.response)
