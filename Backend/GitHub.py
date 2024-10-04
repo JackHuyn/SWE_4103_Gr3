47edc0caf4622d9538782bdd99534187a3f00192
WMazerol <114442203+WMazerol@users.noreply.github.com>
2024-10-04 14:07:06 -0300

    Adds Commit Counter

--- Backend/GitHub.py
+++ Backend/GitHub.py
@@ -27,11 +27,12 @@ class GitHubManager:
                 print(e)
                 author = "None"
             if author not in commit_counts:
-                commit_counts[author] = {'additions': c.stats.additions, 'deletions': c.stats.deletions, 'total': c.stats.total}
+                commit_counts[author] = {'additions': c.stats.additions, 'deletions': c.stats.deletions, 'total': c.stats.total, 'num_commits': 1}
             else:
                 commit_counts[author]['additions'] += c.stats.additions
                 commit_counts[author]['deletions'] += c.stats.deletions
                 commit_counts[author]['total'] += c.stats.total
+                commit_counts[author]['num_commits'] += 1
             # print(author, "\t", c.stats.additions, "\t", c.stats.deletions, "\t", c.stats.total)
         return commit_counts
     
@@ -56,16 +57,16 @@ if __name__ == '__main__':
     g = Github(base_url=GITHUB_BASE_URL)
 
     swe_repo = g.get_repo("JackHuyn/SWE_4103_Gr3")
-    open_issues = swe_repo.get_issues(state='open')
-    print(swe_repo.name)
-    print("---------------------------")
+    # open_issues = swe_repo.get_issues(state='open')
+    # print(swe_repo.name)
+    # print("---------------------------")
 
-    for issue in open_issues:
-        print(issue)
-        print(issue.assignee)
-        print(issue.state)
-        for comment in issue.get_comments():
-            print(comment.body)
+    # for issue in open_issues:
+    #     print(issue)
+    #     print(issue.assignee)
+    #     print(issue.state)
+    #     for comment in issue.get_comments():
+    #         print(comment.body)
 
     print("---------------------------")
 
@@ -73,21 +74,7 @@ if __name__ == '__main__':
     # for c in contributors:
     #     print(c)
 
-    commits = swe_repo.get_commits()
-    commit_counts = {}
-    for c in commits:
-        try:
-            author = c.commit.author.name
-        except Exception as e:
-            print(e)
-            author = "None"
-        if author not in commit_counts:
-            commit_counts[author] = {'additions': c.stats.additions, 'deletions': c.stats.deletions, 'total': c.stats.total}
-        else:
-            commit_counts[author]['additions'] += c.stats.additions
-            commit_counts[author]['deletions'] += c.stats.deletions
-            commit_counts[author]['total'] += c.stats.total
-        # print(author, "\t", c.stats.additions, "\t", c.stats.deletions, "\t", c.stats.total)
+    repo_obj = GitHubManager('JackHuyn/SWE_4103_Gr3')
 
 
     print("---------------------------")
