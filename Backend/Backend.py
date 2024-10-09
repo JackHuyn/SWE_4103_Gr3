618c964829befc6e893834addb8d9502e1192e91
jackhuynh <jackhuynh.ca@gmail.com>
2024-10-09 10:21:39 -0300

    add brennan branch to mine

--- Backend/Backend.py
+++ Backend/Backend.py
@@ -21,10 +21,8 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 FIREBASE_WEB_API_KEY = 'AIzaSyD-f3Vq6kGVXcfjnMmXFuoP1T1mRx7VJXo'
-credFileName = "swe4103-7b261-firebase-adminsdk-7nzkx-e88172454d.json"
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-cred = credentials.Certificate(dir_path + "\\" + credFileName)
+cred = credentials.Certificate(file_path)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -109,7 +107,8 @@ def logout_user():
 def show_courses():
     # get student id
     student_id = request.args.get("studentId", default = -1, type = int)
-    print("id")
+
+    # handle wrong student id case
     if student_id == -1:
         response = app.response_class(
             response=json.dumps({}),
@@ -118,39 +117,44 @@ def show_courses():
         )
         return "Need student id"
     try:  
-        # student_ref = db.collection('student').document(student_id)
-        # student_doc = student_ref.get()
-        # response = app.response_class(
-        #     response=json.dumps({'approved': True}),
-        #     status = 200,
-        #     mimetype='applicaion/json'
-        # )
+        # get student data
+        student_ref = db.collection('student').document(student_id)
+        student_doc = student_ref.get()
+        response = app.response_class(
+            response=json.dumps({'approved': True}),
+            status = 200,
+            mimetype='applicaion/json'
+        )
 
-        # if student_doc.exists:
-        #     student_data = student_doc.to_dict()
-        #     response = app.response_class(
-        #       response=json.dumps({'approved':True}),
-        #       status = 200,
-        #       mimetype='applicaion/json'
-        #       )
-        #     print("data")
-        #     courses = student_data.get('courses',[])
-        #     return "courses"
-        # else:
-        #     response = app.response_class(
-    #           response=json.dumps({'approved':False, 'reason':'No data found'}),
-        #       status = 401,
-        #       mimetype='applicaion/json'
-        #       )
-        #     return "student data not found"
+        # if data exist?
+        if student_doc.exists:
+            student_data = student_doc.to_dict()
 
-        student_node = student_list.find_student(student_id)
-        if student_node:
-            courses = student_node.courses
-            print("Courses:", courses)
-            return jsonify({"studentId": student_id, "courses": courses}), 200
+            response = app.response_class(
+              response=json.dumps({'approved':True}),
+              status = 200,
+              mimetype='applicaion/json'
+              )
+            print("data")
+
+            courses = student_data.get('courses',[])
+            return "courses"
         else:
-            return "Student data not found", 404
+            # handle no data exist
+            response = app.response_class(
+              response=json.dumps({'approved':False, 'reason':'No data found'}),
+              status = 401,
+              mimetype='applicaion/json'
+              )
+            return "student data not found"
+
+        # student_node = student_list.find_student(student_id)
+        # if student_node:
+        #     courses = student_node.courses
+        #     print("Courses:", courses)
+        #     return jsonify({"studentId": student_id, "courses": courses}), 200
+        # else:
+        #     return "Student data not found", 404
         
     except Exception as e:
         response = app.response_class(
