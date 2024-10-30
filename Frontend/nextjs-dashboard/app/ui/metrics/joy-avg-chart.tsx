97dff9e11d833d0cdf23ff483d6a869f6f8557e1
WMazerol <114442203+WMazerol@users.noreply.github.com>
2024-10-30 15:58:09 -0300

    Load Graphs onMount

--- Frontend/nextjs-dashboard/app/ui/metrics/joy-avg-chart.tsx
+++ Frontend/nextjs-dashboard/app/ui/metrics/joy-avg-chart.tsx
@@ -1,100 +1,103 @@
 import Chart from 'chart.js/auto'
+import { useEffect } from 'react'
 
 const groupId = 'TEMPLATE'
 
-async function getChart() {
-    console.log('GET CHART CALLED')
-    let contributions = []
-    return fetch("http://127.0.0.1:3001/metrics/get-avg-team-joy-ratings?groupId="+groupId,
-        {
-            method: 'GET'
-        }
-    ).then(response => {
-        if (!response.ok) {
-            if (response.status === 404) {
-                return {
-                    text: "Server not found!",
-                    status: "danger"
-                };
-            }
-  
-            return response.text().then(text => {
-                return {
-                    text: text,
-                    status: "danger"
-                };
-            })
-        }
-        return response.text().then(text => {
-            return {
-                text: text,
-                status: "success"
-            };
-        })
-    }).then(resp => {
-        console.log("result:", resp);
-        try {
-            let r = JSON.parse(resp.text)
-            if(!r['approved'])
-                throw 'idek bro'
-            console.log(r)
-            //let weeks = []
-            const contributions = r['joyData']
-            const days = contributions.map(row => row.date)
-
-            let avg_joy_graph_data = 
-                [
-                    {
-                        label: 'AVG',
-                        data: contributions.map(row => row.avg)
-                    }
-                ]
-
-            const config = {
-                type: 'line',
-                data: {
-                    labels: days,
-                    datasets: avg_joy_graph_data
-                },
-                options: {
-                    indexAxis: 'x',
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    },
-                    responsive: true,
-                    plugins: {
-                      legend: {
-                        position: 'none',
-                      },
-                      title: {
-                        display: false,
-                        text: 'Avg Joy'
-                      }
-                    }
-                  }
-            };
-
-            return new Chart(
-                    document.getElementById('avg-joy-graph'),
-                    config
-                )
-        } catch(err) {
-            console.log(err)
-        }
-    }).catch((error) => {
-        console.log(error)
-    })
-}
+    
 
 export default function ContributionsGraph()
 {
+
+    useEffect(() => {
+        console.log('GET CHART CALLED')
+        let contributions = []
+        return fetch("http://127.0.0.1:3001/metrics/get-avg-team-joy-ratings?groupId="+groupId,
+            {
+                method: 'GET'
+            }
+        ).then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    return {
+                        text: "Server not found!",
+                        status: "danger"
+                    };
+                }
+      
+                return response.text().then(text => {
+                    return {
+                        text: text,
+                        status: "danger"
+                    };
+                })
+            }
+            return response.text().then(text => {
+                return {
+                    text: text,
+                    status: "success"
+                };
+            })
+        }).then(resp => {
+            console.log("result:", resp);
+            try {
+                let r = JSON.parse(resp.text)
+                if(!r['approved'])
+                    throw 'idek bro'
+                console.log(r)
+                //let weeks = []
+                const contributions = r['joyData']
+                const days = contributions.map(row => row.date)
+    
+                let avg_joy_graph_data = 
+                    [
+                        {
+                            label: 'AVG',
+                            data: contributions.map(row => row.avg)
+                        }
+                    ]
+    
+                const config = {
+                    type: 'line',
+                    data: {
+                        labels: days,
+                        datasets: avg_joy_graph_data
+                    },
+                    options: {
+                        indexAxis: 'x',
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        },
+                        responsive: true,
+                        plugins: {
+                          legend: {
+                            position: 'none',
+                          },
+                          title: {
+                            display: false,
+                            text: 'Avg Joy'
+                          }
+                        }
+                      }
+                };
+    
+                return new Chart(
+                        document.getElementById('avg-joy-graph'),
+                        config
+                    )
+            } catch(err) {
+                console.log(err)
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
     return(
         <div id="avg-joy-container">
             <h3>Average Team Joy</h3>
             <div style={{width: '800px'}}><canvas id="avg-joy-graph"></canvas></div>
-            <button onClick={getChart}>test</button>
         </div>
     )
 }
--- Frontend/nextjs-dashboard/app/ui/metrics/joy-student-rating-graph.tsx
+++ Frontend/nextjs-dashboard/app/ui/metrics/joy-student-rating-graph.tsx
@@ -1,114 +1,114 @@
 import Chart from 'chart.js/auto'
+import { useEffect } from 'react'
 
 const group_id = 'TEMPLATE'
 
-async function getChart() {
-    console.log('GET CHART CALLED')
-    let contributions = []
-    return fetch("http://127.0.0.1:3001/metrics/get-student-joy-ratings?groupId="+group_id,
-        {
-            method: 'GET'
-        }
-    ).then(response => {
-        if (!response.ok) {
-            if (response.status === 404) {
-                return {
-                    text: "Server not found!",
-                    status: "danger"
-                };
+export default function ContributionsGraph()
+{
+    useEffect(() => {
+        console.log('GET CHART CALLED')
+        let contributions = []
+        return fetch("http://127.0.0.1:3001/metrics/get-student-joy-ratings?groupId="+group_id,
+            {
+                method: 'GET'
             }
-  
+        ).then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    return {
+                        text: "Server not found!",
+                        status: "danger"
+                    };
+                }
+      
+                return response.text().then(text => {
+                    return {
+                        text: text,
+                        status: "danger"
+                    };
+                })
+            }
             return response.text().then(text => {
                 return {
                     text: text,
-                    status: "danger"
+                    status: "success"
                 };
             })
-        }
-        return response.text().then(text => {
-            return {
-                text: text,
-                status: "success"
-            };
-        })
-    }).then(resp => {
-        console.log("result:", resp);
-        try {
-            let r = JSON.parse(resp.text)
-            if(!r['approved'])
-                throw 'idek bro'
-            console.log(r)
-            
-            const joy_data = r['joyData']
-            const student_ids = joy_data.map(row => row.student_id)
-
-            let joy_graph_data = []
-            joy_graph_data.push({
-                label: 'Joy Rating',
-                data: joy_data.map(row => row.joy_rating)
-            })
-
-            const config = {
-                type: 'bar',
-                data: {
-                    labels: student_ids,
-                    datasets: joy_graph_data
-                },
-                options: {
-                  indexAxis: 'y',
-                  // Elements options apply to all of the options unless overridden in a dataset
-                  // In this case, we are setting the border of each horizontal bar to be 2px wide
-                  elements: {
-                    bar: {
-                      borderWidth: 2,
-                    }
-                  },
-                  layout: {
-                    padding: {
-                        left: 0,
-                        right: 0,
-                        top: 2,
-                        bottom: 2
-                    }
-                  },
-                  scales: {
-                    x: {
-                        beginAtZero: true
-                    }
-                  },
-                  responsive: true,
-                  plugins: {
-                    legend: {
-                      position: 'none',
+        }).then(resp => {
+            console.log("result:", resp);
+            try {
+                let r = JSON.parse(resp.text)
+                if(!r['approved'])
+                    throw 'idek bro'
+                console.log(r)
+                
+                const joy_data = r['joyData']
+                const student_ids = joy_data.map(row => row.student_id)
+    
+                let joy_graph_data = []
+                joy_graph_data.push({
+                    label: 'Joy Rating',
+                    data: joy_data.map(row => row.joy_rating)
+                })
+    
+                const config = {
+                    type: 'bar',
+                    data: {
+                        labels: student_ids,
+                        datasets: joy_graph_data
                     },
-                    title: {
-                      display: false,
-                      text: 'Student Recent Joy'
-                    }
-                  },
-                  maintainAspectRatio: false
-                },
-              };
-            
-            return new Chart(
-                    document.getElementById('student-joy-graph'),
-                    config
-                )
-        } catch(err) {
-            console.log(err)
-        }
-    }).catch((error) => {
-        console.log(error)
-    })
-}
+                    options: {
+                      indexAxis: 'y',
+                      // Elements options apply to all of the options unless overridden in a dataset
+                      // In this case, we are setting the border of each horizontal bar to be 2px wide
+                      elements: {
+                        bar: {
+                          borderWidth: 2,
+                        }
+                      },
+                      layout: {
+                        padding: {
+                            left: 0,
+                            right: 0,
+                            top: 2,
+                            bottom: 2
+                        }
+                      },
+                      scales: {
+                        x: {
+                            beginAtZero: true
+                        }
+                      },
+                      responsive: true,
+                      plugins: {
+                        legend: {
+                          position: 'none',
+                        },
+                        title: {
+                          display: false,
+                          text: 'Student Recent Joy'
+                        }
+                      },
+                      maintainAspectRatio: false
+                    },
+                  };
+                
+                return new Chart(
+                        document.getElementById('student-joy-graph'),
+                        config
+                    )
+            } catch(err) {
+                console.log(err)
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
 
-export default function ContributionsGraph()
-{
     return(
         <div id="avg-joy-container">
             <h3>Recent Student Joy</h3>
             <div style={{width: '800px'}}><canvas id="student-joy-graph"></canvas></div>
-            <button onClick={getChart}>test</button>
         </div>
     )
 }
