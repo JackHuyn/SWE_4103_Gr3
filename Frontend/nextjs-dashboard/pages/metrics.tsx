534f436629e38223ea59e201acd946766e203fca
WMazerol <114442203+WMazerol@users.noreply.github.com>
2024-11-01 17:46:47 -0300

    Fixes Dialog Backdrop on Metrics Page

--- Frontend/nextjs-dashboard/pages/metrics.tsx
+++ Frontend/nextjs-dashboard/pages/metrics.tsx
@@ -4,16 +4,42 @@ import JoyAvgChart from '@/app/ui/metrics/joy-avg-chart'
 import JoyStudentRatingGraph from '@/app/ui/metrics/joy-student-rating-graph'
 import TeamVelocityGraph from '@/app/ui/metrics/team-velocity-graph'
 import TeamVelocityInput from '@/app/ui/metrics/team-velocity-input'
+import '@/app/ui/stylesheets/metrics.css'
+
+
+async function closeDialogs()
+{
+    document.getElementById('metrics-dialog-backdrop').style.display = 'none'
+    document.getElementById('team-velocity-dialog').style.display = 'none'
+    document.getElementById('joy-rating-dialog').style.display = 'none'
+}
 
 export default function Metrics() {
+
+    async function openJoyRatingDialog() {
+        document.getElementById('metrics-dialog-backdrop').style.display = 'block'
+        document.getElementById('joy-rating-dialog').style.display = 'block'
+    }
+    async function openTeamVelocityDialog() {
+        document.getElementById('metrics-dialog-backdrop').style.display = 'block'
+        document.getElementById('team-velocity-dialog').style.display = 'block'
+    }
+
     return (
         <div>
-            <ContributionsGraph />
-            <JoyRatingInput />
-            <JoyAvgChart />
-            <JoyStudentRatingGraph />
-            <TeamVelocityGraph />
+            <div>
+                <button onClick={openJoyRatingDialog}>Open Joy Rating Dialog</button>
+                <button onClick={openTeamVelocityDialog}>Open Team Velocity Dialog</button>
+            </div>
+            <div>
+                <ContributionsGraph />
+                <JoyAvgChart />
+                <JoyStudentRatingGraph />
+                <TeamVelocityGraph />
+            </div>
+            <div id="metrics-dialog-backdrop" onClick={closeDialogs}></div>
             <TeamVelocityInput />
+            <JoyRatingInput />
         </div>
     )
 }
